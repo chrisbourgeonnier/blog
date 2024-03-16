9787982c2c47f7703eea973ed41af562005c3357
Chris Bourgeonnier <chrisbourgeonnier@gmail.com>
2024-03-16 12:51:29 +0700

    Authentication added, using Devise gem, so only the user can create, edit or delete posts

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable,
+  # :registerable,
+  # registerable not used for a personal blog app
+  :recoverable, :rememberable, :validatable
+end
